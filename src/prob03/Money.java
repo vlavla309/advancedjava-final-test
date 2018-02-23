68e92faa36da4094a7e3846a4bd4a0da34df5eea
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-23 13:56:12 +0900

    문제3 Money

--- src/prob03/Money.java
+++ src/prob03/Money.java
@@ -3,6 +3,37 @@ package prob03;
 public class Money {
 
 	private int amount;
-    
+
+	public Money(int amount) {
+		this.amount = amount;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (getClass() != obj.getClass())
+			return false;
+		Money other = (Money) obj;
+		if (amount != other.amount)
+			return false;
+		return true;
+	}
+
+	public Money add(Money other) {
+		return new Money(this.amount + other.amount);
+	}
+	
+	public Money minus(Money other) {
+		return new Money(this.amount - other.amount);
+	}
+	
+	public Money multiply(Money other) {
+		return new Money(this.amount * other.amount);
+	}
+	
+	public Money devide(Money other) {
+		return new Money(this.amount / other.amount);
+	}
+	
+	
     
 }
