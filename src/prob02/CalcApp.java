39aee5f0591d0ef1563449faa0f44f3ead6c1b01
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-23 13:43:53 +0900

    문제2 calc

--- src/prob02/CalcApp.java
+++ src/prob02/CalcApp.java
@@ -7,11 +7,27 @@ public class CalcApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		System.out.print( "두 정수와 연산자를 입력하시오 >> " );
-		
+
 		int a = scanner.nextInt();
 		int b = scanner.nextInt();
 		String operation = scanner.next();
-		
+
 		/* operation에 따라 4칙 연산 객체를 생성하고 caculate 메서드를 호출합니다. */
+		Arithmetic arith=null;
+		switch (operation) {
+		case "+":
+			arith=new Add();
+			break;
+		case "-":
+			arith=new Sub();
+			break;
+		case "*":
+			arith=new Mul();
+			break;
+		case "/":
+			arith=new Div();
+			break;
+		}
+		System.out.println(arith.calculate(a, b));
 	}
 }
